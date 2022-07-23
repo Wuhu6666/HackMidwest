2dc511871036d84684e4d46290554ba5fa749f00
Wuhu6666 <78830820+Wuhu6666@users.noreply.github.com>
2022-07-23 16:35:08 -0500

    new things

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is our icon</Text>
+      <Button
+        title="Sign In"
+      //Press navigate to another screen
+      />
+
+      <Button
+        title="Sign Up"
+      //Press navigate to another screen
+      />
       <StatusBar style="auto" />
     </View>
   );
